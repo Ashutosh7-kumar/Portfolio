33f50750dc33e6e24bc739607be22534b9761864
Ashutosh kumar <ashutoshkumar7044709@gmail.com>
2025-02-26 14:15:26 +0530

    Update Project.jsx

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Project.css"; // Import the CSS file
+import "./project.css"; // Import the CSS file
 import { motion } from "framer-motion";
 
 const projects = [
